12fb7e53aa0810c394a80784b02b563468813043
Andatra01 <domoyashik@mail.ru>
2026-08-13 01:09:31 +0800

    fix(03-docker): multistage build, размер образа -70%

--- 03-docker/express-app/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('OK'));
-app.get('/health', (req, res) => res.json({status: 'ok'}));
-
-app.listen(3000, () => console.log('Listening on 3000'));
