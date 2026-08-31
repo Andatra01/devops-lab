403b731834db3ed92c5673d8d859d2a50aea67b8
Andatra01 <domoyashik@mail.ru>
2026-08-31 23:07:39 +0800

    create: 12-hw

--- 03-docker/app.py
+++ 03-docker/app.py
@@ -1,15 +1,6 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import json
+class Server:
+    def __init__(self, name, ip):
+        self.name = name
+        self.ip = ip
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/health':
-            body = json.dump({"status": "ok"}).encode()
-        else:
-            body = b"Hello World!"
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(body)
-
-port = 8000
-HTTPServer(('', port), Handler).serve_forever()
+s = Server("web1", "192.168.1.10")
